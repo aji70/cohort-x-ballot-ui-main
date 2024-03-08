719582b7498d359bd1a418d79e39333e8eb71109
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-03-09 00:30:15 +0100

    Ballot

--- src/hooks/useProposals.js
+++ src/hooks/useProposals.js
@@ -4,33 +4,33 @@ import { readOnlyProvider } from "../constants/providers";
 import { decodeBytes32String } from "ethers";
 
 const useProposals = () => {
-    const [proposals, setProposal] = useState({
-        loading: true,
-        data: [],
-    });
+  const [proposals, setProposal] = useState({
+    loading: true,
+    data: [],
+  });
 
-    useEffect(() => {
-        const contract = getProposalsContract(readOnlyProvider);
+  useEffect(() => {
+    const contract = getProposalsContract(readOnlyProvider);
 
-        contract
-            .getAllProposals()
-            .then((res) => {
-                const converted = res.map((item) => ({
-                    name: decodeBytes32String(item.name),
-                    voteCount: item.voteCount,
-                }));
-                setProposal({
-                    loading: false,
-                    data: converted,
-                });
-            })
-            .catch((err) => {
-                console.error("error fetching proposals: ", err);
-                setProposal((prev) => ({ ...prev, loading: false }));
-            });
-    }, []);
+    contract
+      .getAllProposals()
+      .then((res) => {
+        const converted = res.map((item) => ({
+          name: decodeBytes32String(item.name),
+          voteCount: item.voteCount,
+        }));
+        setProposal({
+          loading: false,
+          data: converted,
+        });
+      })
+      .catch((err) => {
+        console.error("error fetching proposals: ", err);
+        setProposal((prev) => ({ ...prev, loading: false }));
+      });
+  }, []);
 
-    return proposals;
+  return proposals;
 };
 
 export default useProposals;
